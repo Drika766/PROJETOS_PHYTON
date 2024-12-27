d573cd1734c6d726fdb2d98f31de5eb9e59d12e5
Adriana de Fátima <adrianadefatimamartins30@gmail.com>
2024-12-27 19:23:10 -0300

    Inclusão

--- pesquisa_google.py
+++ pesquisa_google.py
@@ -6,21 +6,37 @@ import time
 # Inicializa o navegador
 navegador = webdriver.Chrome()
 
-# Abre o Google
-navegador.get("https://www.google.com.br")
+try:
+    # Abre o Google
+    navegador.get("https://www.google.com.br")
 
-# Maximiza a janela do navegador
-navegador.maximize_window()
+    # Maximiza a janela do navegador
+    navegador.maximize_window()
 
-# Encontra a barra de pesquisa e insere o termo
-barra_pesquisa = navegador.find_element(By.NAME, "q")
-barra_pesquisa.send_keys("tutorial de Robot Framework")
+    # Encontra a barra de pesquisa e insere o termo
+    barra_pesquisa = navegador.find_element(By.NAME, "q")
+    barra_pesquisa.send_keys("tutorial de Robot Framework")
 
-# Simula a tecla Enter ou clica no botão pesquisar
-barra_pesquisa.send_keys(Keys.RETURN)
+    # Simula a tecla Enter ou clica no botão pesquisar
+    barra_pesquisa.send_keys(Keys.RETURN)
 
-# Aguarda alguns segundos para ver os resultados
-time.sleep(10)
+    # Aguarda alguns segundos para ver os resultados
+    time.sleep(5)
 
-# Fecha o navegador
-navegador.quit()
+    # Abre uma nova aba e acessa o formulário
+    navegador.execute_script("window.open('https://www.hastagtreinamentos.com/curso-python', '_blank');")
+
+    # Troca para a nova aba
+    navegador.switch_to.window(navegador.window_handles[1])
+
+    # Preenche o formulário
+    navegador.find_element(By.ID, "firstname").send_keys("Adriana de Fatima")
+    navegador.find_element(By.ID, "email").send_keys("adrianadefatimamartins30@gmail.com")
+    navegador.find_element(By.ID, "phone").send_keys("61981576142")
+
+    # Aguarda alguns segundos para ver os resultados
+    time.sleep(5)
+
+finally:
+    # Fecha o navegador
+    navegador.quit()
--- teste.robot
+++ teste.robot
@@ -5,7 +5,16 @@ Library    SeleniumLibrary
 Pesquisar Tutorial de Robot Framework
     Open Browser    https://www.google.com.br    chrome
     Maximize Browser Window
-    Input Text    name:q    tutorial de Robot Framework
-    Press Key    name:q    \\13  # Simula o Enter
+    Input Text    name=q    tutorial de Robot Framework
+    Press Key    name=q    \\13  # Simula o Enter
     Sleep    10
+    # Abre uma nova aba e acessa o formulário
+    Execute Javascript    window.open('https://www.hastagtreinamentos.com/curso-python', '_blank');
+    # Troca para a nova aba
+    Switch Window    2
+    # Preenche o formulário
+    Input Text    id=firstname    Adriana de Fatima
+    Input Text    id=email    adrianadefatimamartins30@gmail.com
+    Input Text    id=phone    999999999
+    Sleep    5
     Close Browser
